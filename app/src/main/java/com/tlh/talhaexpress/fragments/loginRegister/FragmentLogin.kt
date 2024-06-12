19b578616bc7fd8f2e45ea14bd9a5f81ada2d941
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-06-12 21:16:07 +0300

    gradle to groovy migration

--- app/src/main/java/com/tlh/talhaexpress/fragments/loginRegister/FragmentLogin.kt
+++
@@ -1,7 +0,0 @@
-package com.tlh.talhaexpress.fragments.loginRegister
-
-import androidx.fragment.app.Fragment
-import com.tlh.talhaexpress.R
-
-class FragmentLogin: Fragment(R.layout.fragment_login) {
-}
