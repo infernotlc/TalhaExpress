80da5175277f10cf82f1d5d41e64e87800081db2
infernotlc <talha5797@gmail.com>
2024-07-14 14:04:25 +0300

    adapters added

--- app/src/main/java/com/tlh/talhaexpress3/activities/ShoppingActivity.kt
+++ app/src/main/java/com/tlh/talhaexpress3/activities/ShoppingActivity.kt
@@ -2,30 +2,51 @@ package com.tlh.talhaexpress3.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import com.tlh.talhaexpress3.R
 import com.tlh.talhaexpress3.databinding.ActivityShoppingBinding
+import com.tlh.talhaexpress3.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
 
-    private val binding by lazy {
+    val binding by lazy {
         ActivityShoppingBinding.inflate(layoutInflater)
     }
 
+    val viewModel by viewModels<CartViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
 
-       val navController = findNavController(R.id.shoppingHostFragment)
+        val navController = findNavController(R.id.shoppingHostFragment)
         binding.bottomNavigation.setupWithNavController(navController)
 
-
+        lifecycleScope.launchWhenStarted {
+            viewModel.cartProducts.collectLatest {
+                when (it) {
+                    is Resource.Success<*> -> {
+                        val count = it.data?.size ?: 0
+                        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+                        bottomNavigation.getOrCreateBadge(R.id.cartFragment).apply {
+                            number = count
+                            backgroundColor = resources.getColor(R.color.g_blue)
+                        }
+                    }
+                    else -> Unit
+                }
+            }
+        }
     }
+
 }
---
+++ app/src/main/java/com/tlh/talhaexpress3/adapters/AddressAdapter.kt
@@ -0,0 +1,76 @@
+package com.tlh.talhaexpress3.adapters
+
+import android.graphics.drawable.ColorDrawable
+import android.location.Address
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+
+class AddressAdapter : RecyclerView.Adapter<AddressAdapter.AddressViewHolder>() {
+
+    inner class AddressViewHolder(val binding: AddressRvItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(address: Address, isSelected: Boolean) {
+            binding.apply {
+                buttonAddress.text = address.addressTitle
+                if (isSelected){
+                    buttonAddress.background = ColorDrawable(itemView.context.resources.getColor(R.color.g_blue))
+                }else{
+                    buttonAddress.background = ColorDrawable(itemView.context.resources.getColor(R.color.g_white))
+                }
+            }
+        }
+
+
+    }
+
+    private val diffUtil by lazy {
+        object : DiffUtil.ItemCallback<Address>() {
+        override fun areItemsTheSame(oldItem: Address, newItem: Address): Boolean {
+            return oldItem.addressTitle == newItem.addressTitle && oldItem.fullName == newItem.fullName
+        }
+
+        override fun areContentsTheSame(oldItem: Address, newItem: Address): Boolean {
+            return oldItem == newItem
+        }
+    }
+    }
+    val differ = AsyncListDiffer(this, diffUtil)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressViewHolder {
+        return AddressViewHolder(
+            AddressRvItemBinding.inflate(
+                LayoutInflater.from(parent.context)
+            )
+        )
+    }
+
+    var selectedAddress = -1
+    override fun onBindViewHolder(holder: AddressViewHolder, position: Int) {
+        val address = differ.currentList[position]
+        holder.bind(address, selectedAddress == position)
+
+        holder.binding.buttonAddress.setOnClickListener {
+            if (selectedAddress >= 0)
+                notifyItemChanged(selectedAddress)
+            selectedAddress = holder.adapterPosition
+            notifyItemChanged(selectedAddress)
+            onClick?.invoke(address)
+        }
+    }
+
+    init {
+        differ.addListListener { _, _ ->
+            notifyItemChanged(selectedAddress)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+
+    var onClick: ((Address) -> Unit)? = null
+}
